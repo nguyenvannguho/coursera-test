b132caf93369679cbaab015157e3f520226338eb
dannynguyen <nguyenvannguho@gmail.com>
2016-10-30 07:56:26 -0700

    Update fix bug http

--- module5-solution/Lecture59/view/component/controller/dataServiceProcessing.js
+++ module5-solution/Lecture59/view/component/controller/dataServiceProcessing.js
@@ -8,7 +8,7 @@ DataServiceProcessing.$inject = ['$http'];
 function DataServiceProcessing($http) {
   var service = this;
   service.getShortName = function (short_name) {
-    return $http.get('http://dannynguyen-course5.herokuapp.com/menu_items/' + short_name + '.json')
+    return $http.get('https://dannynguyen-course5.herokuapp.com/menu_items/' + short_name + '.json')
                   .then(function(response){
                     return response.data;
                   }, function(errResponse){
