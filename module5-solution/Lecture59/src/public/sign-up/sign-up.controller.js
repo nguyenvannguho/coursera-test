c1aa500aae3d23b48eebfdd14560f60f08479792
dannynguyen <nguyenvannguho@gmail.com>
2016-10-30 08:09:08 -0700

    update bug fix

--- module5-solution/Lecture59/src/public/sign-up/sign-up.controller.js
+++ module5-solution/Lecture59/src/public/sign-up/sign-up.controller.js
@@ -7,26 +7,26 @@ angular.module('public')
 SignUpController.$inject = ['DataServiceProcessing', 'StoreDataService', '$scope'];
 function SignUpController(DataServiceProcessing, StoreDataService, $scope) {
   var signup = this;
-  signup.firstName = 'A';
-  signup.lastName = 'A';
-  signup.email = 'A@a';
-  signup.phone = '111-111-1111';
-  signup.shortName = 'A1';
+  signup.firstName = '';
+  signup.lastName = '';
+  signup.email = '';
+  signup.phone = '';
+  signup.shortName = '';
   signup.dataShortName = '';
   signup.showErrorShortName = false;
   signup.submitSuccess = false;
 
-  // signup.initialize = function () {
-  //   if (StoreDataService.checkDataRegister) {
-  //     var data = StoreDataService.getDataRegister();
-  //     signup.firstName = data.firstName;
-  //     signup.lastName = data.lastName;
-  //     signup.email = data.email;
-  //     signup.phone = data.phone;
-  //     signup.shortName = data.shortName;
-  //     signup.dataShortName = data.dataShortName;
-  //   }
-  // }
+  signup.initialize = function () {
+    if (StoreDataService.checkDataRegister() === true) {
+      var data = StoreDataService.getDataRegister();
+      signup.firstName = data.firstName;
+      signup.lastName = data.lastName;
+      signup.email = data.email;
+      signup.phone = data.phone;
+      signup.shortName = data.shortName;
+      signup.dataShortName = data.dataShortName;
+    }
+  };
   signup.loseFocus = function () {
     DataServiceProcessing.getShortName(signup.shortName).then(function(data) {
       if(data === 404 || data === 500) {
@@ -70,7 +70,8 @@ function SignUpController(DataServiceProcessing, StoreDataService, $scope) {
       });
     }
   };
-  //signup.initialize();
+
+  signup.initialize();
 }
 
 })();
